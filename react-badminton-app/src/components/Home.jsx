05c0ef8ac75e0f546ae799d77dca080e6adb8351
Aaron <aaron.shah.18.112@gmail.com>
2024-11-01 16:53:56 +0000

    Added alt text to help with accessibility

--- react-badminton-app/src/components/Home.jsx
+++ react-badminton-app/src/components/Home.jsx
@@ -3,11 +3,11 @@ import logo from '/logo-SVG.svg';
 export default function Home() {
     return (
         <div>
-            <header className="svg-container">
-                <img alt="Logo" src={logo} width={100} height={100} />
+            <header className="svg-container" role="banner" aria-label="Application logo">
+                <img alt="AJ Bell Logo" src={logo} width={100} height={100}/>
             </header>
 
-            <nav className="button-container">
+            <nav className="button-container" role="navigation" aria-label="Main Navigation">
                 <button aria-label="Numbers" id="numbers">Numbers</button>
                 <button aria-label="Payment" id="payment">Payment</button>
                 <button aria-label="Admin" id="admin">Admin</button>
