8491c24846dc06f3cbbe67203faaaebdc2aab733
Aaron <aaron.shah.18.112@gmail.com>
2024-11-01 10:07:47 +0000

    vite

---
+++ vite-badminton-app/src/App.js
@@ -0,0 +1,13 @@
+import AJBLogo from '/logo-SVG.svg';
+
+function App() {
+    return (
+        <>
+            <div className="svg-container">
+                <img src={AJBLogo} alt="Logo" />
+            </div>
+        </>
+    );
+}
+
+export default App;
