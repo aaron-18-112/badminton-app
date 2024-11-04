5576c2af6ee1312d59c6e9b3624ea4f2f8418b58
Aaron <aaron.shah.18.112@gmail.com>
2024-11-04 15:54:47 +0000

    changes with flexbox

--- next-badminton-app/app/page.js
+++ next-badminton-app/app/page.js
@@ -2,20 +2,21 @@ import Image from 'next/image';
 import './globals.css';
 
 export default function Home() {
+
     return (
-        <div>
+        <div className="home-root">
             <header className="svg-container">
                 <Image alt="Logo" src="logo-SVG.svg" width={100} height={100}/>
+                <nav className="button-container" aria-label="Main Navigation">
+                    <button aria-label="Numbers" id="numbers">Numbers</button>
+                    <button aria-label="Payment" id="payment">Payment</button>
+                    <button aria-label="Admin" id="admin">Admin</button>
+                </nav>
             </header>
-
-            <nav className="button-container" aria-label="Main Navigation">
-                <button aria-label="Numbers" id="numbers">Numbers</button>
-                <button aria-label="Payment" id="payment">Payment</button>
-                <button aria-label="Admin" id="admin">Admin</button>
-            </nav>
-
-
             <main id="main-content" role="main"></main>
         </div>
-    );
+
+
+    )
+        ;
 }
