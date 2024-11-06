ed9a789de63c6cad7cddefde2a64b9b9ee7858a9
Aaron <aaron.shah.18.112@gmail.com>
2024-11-06 09:53:57 +0000

    New layout, transferred html to the nextjs file

--- next-badminton-app/app/page.js
+++ next-badminton-app/app/page.js
@@ -1,22 +1,46 @@
-import Image from 'next/image';
+"use client";
+
 import './globals.css';
+import {useEffect, useState} from "react";
 
 export default function Home() {
 
+    const [date, setDate] = useState(null);
+    useEffect(() => {
+        const d = new Date(2024, 10, 12);
+        setDate(d);
+    }, []);
+
+    const formattedDate = date
+        ? date.toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit', year: '2-digit'})
+        : 'Loading...';
+
     return (
-        <div className="home-root">
-            <header className="svg-container">
-                <Image alt="Logo" src="logo-SVG.svg" width={100} height={100}/>
-                <nav className="button-container" aria-label="Main Navigation">
+        <div className="page">
+
+            <div className="sidebar">
+
+                <img className="svg-container" alt="Logo" src="logo-SVG.svg"/>
+
+                <nav className="button-container">
                     <button aria-label="Numbers" id="numbers">Numbers</button>
                     <button aria-label="Payment" id="payment">Payment</button>
                     <button aria-label="Admin" id="admin">Admin</button>
                 </nav>
-            </header>
-            <main id="main-content" role="main"></main>
+
+            </div>
+
+
+            <div className="content-container">
+                <section className="content1">
+                    <h4>Next Badminton Session: {formattedDate}</h4>
+                    <h4>Total Number of Players: XXX</h4>
+                </section>
+                <button className="content2">Join</button>
+                <div className="content3">Content 3</div>
+            </div>
+
         </div>
-
-
     )
         ;
 }
