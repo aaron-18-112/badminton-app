fd3fe2f15640aeb40b373ea2de4602870c49f409
Aaron <aaron.shah.18.112@gmail.com>
2024-11-08 14:31:50 +0000

    Admin button removal

--- angular-badminton-app/src/app/components/hamburger-menu/hamburger-menu.component.ts
+++ angular-badminton-app/src/app/components/hamburger-menu/hamburger-menu.component.ts
@@ -1,10 +1,11 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {NgClass} from '@angular/common';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
     selector: 'app-hamburger-menu',
     standalone: true,
-    imports: [NgClass],
+    imports: [NgClass, RouterLink, RouterLinkActive],
     templateUrl: `hamburger-menu.component.html`,
     styleUrls: ['hamburger-menu.component.css']
 })
