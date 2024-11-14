8a04b7ef66fe7e1dd261e4435d42b5518df1411a
Aaron <137836369+aaron-18-112@users.noreply.github.com>
2024-11-14 17:22:25 +0000

    Wrote some e2e tests in Playwright

--- final-apps/player-badminton-app/src/app/components/payment-amount/payment-amount.component.ts
+++ final-apps/player-badminton-app/src/app/components/payment-amount/payment-amount.component.ts
@@ -1,12 +1,9 @@
 import {Component} from '@angular/core';
-import {DateAndTimeComponent} from "../date-and-time/date-and-time.component";
 
 @Component({
     selector: 'app-payment-amount',
     standalone: true,
-    imports: [
-        DateAndTimeComponent
-    ],
+    imports: [],
     templateUrl: 'payment-amount.component.html',
     styleUrls: ['payment-amount.component.css']
 })
