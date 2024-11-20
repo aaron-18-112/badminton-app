c0e559812538f4a2940e1faa9c0839ec05bf3e10
Aaron <137836369+aaron-18-112@users.noreply.github.com>
2024-11-20 14:02:27 +0000

    rebuilt API

---
+++ final-apps/backend/BadmintonApi/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace BadmintonApi.Models;
+
+public class Player
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string firstName { get; set; } = null!;
+    public string lastName { get; set; } = null!;
+    public string email { get; set; } = null!;
+}
---
+++ final-apps/backend/BadmintonApi/Services/MongoDBService.cs
@@ -0,0 +1,35 @@
+﻿using BadmintonApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace BadmintonApi.Services;
+
+public class MongoDBService
+{
+    private readonly IMongoCollection<Player> _playerCollection;
+    public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings) {
+        MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+        IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+        _playerCollection = database.GetCollection<Player>(mongoDBSettings.Value.CollectionName);
+    }
+
+    public async Task<List<Player>> GetAsync()
+    {
+        return await _playerCollection.Find(new BsonDocument()).ToListAsync();
+    }
+    
+    public async Task CreateAsync(Player player)
+    {
+        await _playerCollection.InsertOneAsync(player);
+        return;
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        FilterDefinition<Player> filter = Builders<Player>.Filter.Eq("Id", id);
+        await _playerCollection.DeleteOneAsync(filter);
+        return;
+    }
+
+}
